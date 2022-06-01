014f7bafdb892de7c34f9f9ebd6088eb6f8bf31c
GeorgeMBS <ilustrageorge@gmail.com>
2022-05-31 21:59:24 -0300

    att aula 14

---
+++ aula 14/repetições.js
@@ -0,0 +1,22 @@
+// var c = 1
+// while (c <= 5) {
+//     console.log(c)      WHILE
+//     c++
+// }
+
+//==============================================
+
+// var c = 1
+// do {
+//     console.log(c)     DO WHILE
+//     c++
+// } while (c <= 5)
+
+//==============================================
+
+console.log('Vai Começar...')
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+
+console.log('FIM!')
