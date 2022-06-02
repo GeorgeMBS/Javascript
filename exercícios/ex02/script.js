cd35b67ef155a55d568828ba1791c3c48f5928c3
GeorgeMBS <ilustrageorge@gmail.com>
2022-06-02 12:28:42 -0300

    atualização

--- exercícios/ex02/script.js
+++ exercícios/ex02/script.js
@@ -1,26 +1,18 @@
-function carregar() {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-var minutos = data.getMinutes()
-
-msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos!`
-
-if (hora >= 0 && hora < 12){
-    //BOM DIA!
-    img.src = 'imagens/manhãpq.jpg'
-    document.body.style.background = '#C5732D'
-    frase.innerHTML = 'BOM DIA!'
-} else if (hora >= 12 && hora < 18) {
-    // BOA TARDE!
-    img.src = 'imagens/tardepq.jpg'
-    document.body.style.background = '#7C7A5E'
-    frase.innerHTML = 'BOA TARDE!'
-} else {
-    // BOA NOITE!
-    img.src = 'imagens/noitepq.jpg'
-    document.body.style.background = '#181F29'
-    frase.innerHTML = 'BOA NOITE!'
-}
+function tabuada () {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
